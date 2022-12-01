139ce6e4d38bd26883944b9b02a926c11c71b7d4
theglassofwater <joeace2002@gmail.com>
2022-12-01 23:07:02 +0000

    set up simple web pages

--- com2025_project/WebApp/views.py
+++ com2025_project/WebApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def home(response):
+	return HttpResponse("home")
+
+
+def contact(response):
+	return HttpResponse("contact")
+
+def resource(response):
+	return HttpResponse("resource 1")
